11939e74d50ee96d9dbead0261f9e0c73f26816f
Yuanhai <Shi>
2016-04-13 22:20:47 -0400

    [simple-todos]Add CRUD implementation for tasks.

--- simple-todos/imports/ui/body.js
+++ simple-todos/imports/ui/body.js
@@ -1,11 +1,27 @@
 import {Template} from 'meteor/templating';
+import {Tasks} from '../api/tasks.js';
 
+import './task.js';
 import './body.html';
 
 Template.body.helpers({
-	tasks: [
-		{text: 'This is task 1'},
-		{text: 'This is task 2'},
-		{text: 'This is task 3'},
-	],
+	tasks() {
+		return Tasks.find({}, {sort: {createdAt: -1}});
+	},
+});
+
+Template.body.events({
+	//listen to submit event which matches css selector
+	'submit .new-task'(event) {
+		event.preventDefault();
+		const target = event.target;
+		const text = target.text.value;
+
+		Tasks.insert({
+			text,
+			createdAt: new Date(),
+		});
+
+		target.text.value = "";
+	},
 });
